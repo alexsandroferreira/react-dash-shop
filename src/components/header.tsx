bc110c39f894018475d10fe46d87518300077db2
alexsandroferreira <ferreira.alex@outlook.com>
2024-07-15 11:29:15 -0300

    feat: add page orders

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ export function Header() {
             Inicio
           </NavLink>
 
-          <NavLink to="/ordens">
+          <NavLink to="/orders">
             <UtensilsCrossed className="h-4 w-4" />
             Pedidos
           </NavLink>
