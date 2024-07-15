0c4e313c7174819e5ef39c791c95271da7ce4ae2
alexsandroferreira <ferreira.alex@outlook.com>
2024-07-15 07:41:50 -0300

    feat: add themeToggle and account-menu components

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster richColors closeButton />
-      <Helmet titleTemplate="%s | frontend-template" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="deliver-shop-theme" defaultTheme="dark">
+        <Toaster richColors closeButton />
+        <Helmet titleTemplate="%s | frontend-template" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
