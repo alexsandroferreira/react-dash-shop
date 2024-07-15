2b722463d80bfb906852570759487d4aa0bb1ef9
alexsandroferreira <ferreira.alex@outlook.com>
2024-07-14 21:30:52 -0300

    feat: add page sing-Up

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SingIn } from './pages/auth/sing-in'
+import { SingUp } from './pages/auth/sing-up'
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sing-in', element: <SingIn /> }],
+    children: [
+      { path: '/sing-in', element: <SingIn /> },
+      { path: '/sing-up', element: <SingUp /> },
+    ],
   },
 ])
